8a652e223f796d61bd826e3bc173b55f19cc9b82
Brett Haines <bhaines3@gatech.edu>
2018-11-27 19:56:38 -0500

    Adding main menu scene and script

---
+++ scripts/main_menu.gd
@@ -0,0 +1,33 @@
+extends Node
+
+# Button objects
+onready var cont_btn = get_node("menu_container/cont_button")
+onready var level_btn = get_node("menu_container/level_button")
+onready var opt_btn = get_node("menu_container/opt_button")
+onready var quit_btn = get_node("menu_container/quit_button")
+
+func _ready():
+	# Connect buttons to respective functions
+	cont_btn.connect("pressed", self, "return_level", [])
+	level_btn.connect("pressed", self, "choose_level", [])
+	opt_btn.connect("pressed", self, "open_options", [])
+	quit_btn.connect("pressed", self, "quit_game", [])
+
+# Return to last unsolved puzzle
+func return_level():
+	# TODO: Implement this
+	pass
+
+# Choose level number from list
+func choose_level():
+	# TODO: Implement level picking menu
+	pass
+
+# Open the options menu
+func open_options():
+	# TODO: Implement options menu
+	pass
+
+# Close the game
+func quit_game():
+	get_tree().quit()
