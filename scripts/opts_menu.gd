ff69ea9d84265816fb1825c208a5b1f41a79425a
Brett Haines <bhaines3@gatech.edu>
2018-12-02 14:55:55 -0500

    Adding additional options

--- scripts/opts_menu.gd
+++ scripts/opts_menu.gd
@@ -8,25 +8,31 @@ const opts_filepath = "user://options.json"
 
 # GUI objects
 onready var back_btn = get_node("back_btn")
-onready var opts_sel = get_node("opts_menu_container/difficulty_selector")
+onready var diff_sel = get_node("opts_menu_container/difficulty_selector")
+onready var darkm_check = get_node("opts_menu_container/darkmode_check")
+onready var reset_btn = get_node("opts_menu_container/reset")
 
 # Menu text arrays
 var diff_array = ["Easy", "Medium", "Hard"]
 
 # Saved options
-var pers_opts = {"difficulty":0}
+var pers_opts;
 
 func _ready():
 	# Get saved options (if they exist)
 	if File.new().file_exists(opts_filepath):
 		pers_opts = FileIO.read_json_file(opts_filepath)
 	else:
-		# TODO: Setup default options
-		pass
+		set_defaults()
 	# Connect back button to close function
-	back_btn.connect("pressed", self, "close_menu", [])
-	# Setup difficulty dropdown
+	back_btn.connect("pressed", self, "close_menu")
+	# Setup option UIs
 	setup_diff_picker()
+	darkm_check.set_pressed(pers_opts["darkmode"])
+	# Connect options UIs to respective functions
+	diff_sel.connect("item_selected", self, "on_diff_selected")
+	darkm_check.connect("pressed", self, "on_darkm_selected")
+	reset_btn.connect("pressed", self, "set_defaults")
 
 func close_menu():
 	# Save options
@@ -37,6 +43,22 @@ func close_menu():
 func setup_diff_picker():
 	# Add each difficulty to the dropdown
 	for item in diff_array:
-		opts_sel.add_item(item)
+		diff_sel.add_item(item)
 	# Select saved option
-	opts_sel.select(pers_opts["difficulty"])
+	diff_sel.select(pers_opts["difficulty"])
+
+func on_diff_selected(item):
+	# Save selected difficulty to pers_opts
+	pers_opts["difficulty"] = item
+
+func on_darkm_selected():
+	# Save selected boolean to pers_opts
+	pers_opts["darkmode"] = darkm_check.is_pressed()
+
+func set_defaults():
+	# Write default values to options file
+	pers_opts = { "difficulty":0, "darkmode":false }
+	FileIO.write_json_file(opts_filepath, pers_opts)
+	# Change UI elements to defaults
+	diff_sel.select(pers_opts["difficulty"])
+	darkm_check.pressed = pers_opts["darkmode"]
