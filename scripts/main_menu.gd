372aa708e95c68c6be365be4719e0ff43c9f09b1
Brett Haines <bhaines3@gatech.edu>
2018-12-04 23:17:19 -0500

    Adding button to main menu for records screen

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -6,15 +6,17 @@ const FileIO = preload("res://scripts/file_io.gd")
 # Button objects
 onready var cont_btn = get_node("menu_container/cont_button")
 onready var level_btn = get_node("menu_container/level_button")
+onready var recs_btn = get_node("menu_container/records_button")
 onready var opt_btn = get_node("menu_container/opt_button")
 onready var quit_btn = get_node("menu_container/quit_button")
 
 func _ready():
 	# Connect buttons to respective functions
-	cont_btn.connect("pressed", self, "return_level", [])
-	level_btn.connect("pressed", self, "choose_level", [])
-	opt_btn.connect("pressed", self, "open_options", [])
-	quit_btn.connect("pressed", self, "quit_game", [])
+	cont_btn.connect("pressed", self, "return_level")
+	level_btn.connect("pressed", self, "choose_level")
+	recs_btn.connect("pressed", self, "open_records")
+	opt_btn.connect("pressed", self, "open_options")
+	quit_btn.connect("pressed", self, "quit_game")
 
 # Return to last unsolved puzzle
 func return_level():
@@ -24,6 +26,10 @@ func return_level():
 # Choose level number from list
 func choose_level():
 	get_tree().change_scene("res://scenes/lvl_menu.tscn")
+
+# Open the records list
+func open_records():
+	get_tree().change_scene("res://scenes/records_menu.tscn")
 
 # Open the options menu
 func open_options():
