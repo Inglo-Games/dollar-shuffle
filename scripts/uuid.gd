143d6602cedb34407cc1d6d33d7514fcdc433dc4
Brett Haines <BHaines418@gmail.com>
2019-01-03 22:26:02 -0500

    Cleaning up UUID class

--- scripts/uuid.gd
+++ scripts/uuid.gd
@@ -1,20 +1,16 @@
 extends Node
 
 # Hash the input and make it the seed for the internal RNG
-func setup_seed(input):
+static func setup_seed(input):
 	seed(input.hash())
 
-# Converts an integer into a base 64 string
-func int2base64(num):
-	pass
-
 # Generate a new UUID, with input seed if given
-func gen_uuid(input):
+static func gen_uuid(input):
 	# If input isn't null, make it the seed
-	if input is null:
+	if input != null:
 		setup_seed(input)
 	# Otherwise generate a new random seed
 	else:
 		randomize()
 	# Convert new randi to a base 64 string
-	return int2base64(randi())
+	return Marshalls.variant_to_base64(randi())
