046714fb9bd04e083b496c13d1d655f7278bcbcc
Brett Haines <bhaines3@gatech.edu>
2019-01-05 22:56:02 -0500

    Adding popup for random level selection

---
+++ scripts/rand_popup.gd
@@ -0,0 +1,26 @@
+extends Popup
+
+# Utility classes
+var RNG = preload("res://scripts/rng_seed.gd")
+
+# UI elements
+onready var edit = get_node("vbox/hbox/edit")
+onready var conf_btn = get_node("vbox/hbox/confirm_btn")
+
+func _ready():
+	set_process_input(true)
+	# Connect button to its function
+	conf_btn.connect("pressed", self, "open_level")
+
+func open_level():
+	# If edit is not blank, use contents as RNG seed
+	if edit.text != "":
+		globals.current_level = edit.text
+		RNG.set_seed(edit.text)
+	else:
+		globals.current_level = RNG.gen_seed()
+	# Free this popup from memory
+	self.queue_free()
+	# Load instance of game scene
+	var level_scene = ResourceLoader.load("res://scenes/game.tscn")
+	get_tree().get_root().add_child(level_scene.instance())
