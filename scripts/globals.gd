e461a362188462a2a3a377916e4281d65262fc69
Brett Haines <bhaines3@gatech.edu>
2019-01-12 14:05:54 -0500

    Converting options file to use project settings

--- scripts/globals.gd
+++ scripts/globals.gd
@@ -22,61 +22,89 @@ const BACK_DARK = Color("#FF5B5B5B")
 var current_level = 1
 var number_of_levels = -1
 
-# Persistent options and user data
-var pers_opts;
+# User data file to hold high scores and times
 var user_data;
 
 # Initialization
 func _ready():
-	# Load options data from file (if it exists)
-	if File.new().file_exists(opts_filepath):
-		pers_opts = FileIO.read_json_file(opts_filepath)
-	else:
-		pers_opts = get_options_defaults()
-		FileIO.write_json_file(opts_filepath, pers_opts)
+	# Write game-specific options to project config if they're not already there
+	if !ProjectSettings.has_setting("gui/theme/skin"):
+		var info = {
+			"name":"gui/theme/skin",
+			"type":TYPE_INT,
+			"hint":PROPERTY_HINT_ENUM,
+			"hint_string":"zero,one"
+		}
+		ProjectSettings.set("gui/theme/skin", 0)
+		ProjectSettings.add_property_info(info)
+	if !ProjectSettings.has_setting("game/difficulty"):
+		var info = {
+			"name":"game/difficulty",
+			"type":TYPE_INT,
+			"hint":PROPERTY_HINT_ENUM,
+			"hint_string":"zero,one,two"
+		}
+		ProjectSettings.set("game/difficulty", 0)
+		ProjectSettings.add_property_info(info)
+	if !ProjectSettings.has_setting("game/last_played"):
+		var info = {
+			"name":"game/last_played",
+			"type":TYPE_OBJECT,
+			"hint":PROPERTY_HINT_NONE,
+			"hint_string":""
+		}
+		ProjectSettings.set("game/last_played", 1)
+		ProjectSettings.add_property_info(info)
+	if !ProjectSettings.has_setting("game/tutorial_played"):
+		var info = {
+			"name":"game/tutorial_played",
+			"type":TYPE_BOOL,
+			"hint":PROPERTY_HINT_ENUM,
+			"hint_string":"true,false"
+		}
+		ProjectSettings.set("game/tutorial_played", false)
+		ProjectSettings.add_property_info(info)
+	ProjectSettings.save()
+	# Set current level
+	current_level = ProjectSettings.get_setting("game/last_played")
 	# Load user data from file
 	if File.new().file_exists(user_filepath):
 		user_data = FileIO.read_json_file(user_filepath)
-		current_level = user_data["last_played"]
 	else:
-		user_data = {"last_played":1}
+		user_data = {}
 		FileIO.write_json_file(user_filepath, user_data)
 	# Get number of levels in res://levels directory
 	number_of_levels = count_files("res://levels")
 
 # Global functions
 
-# Return options defalts
-func get_options_defaults():
-	return { "difficulty":0, "skin":0 }
-
 # Update the last played level saved to userdata file
 func update_last_level():
 	# Update variable and write to file
-	user_data["last_played"] = current_level
-	FileIO.write_json_file(user_filepath, user_data)
+	ProjectSettings.set("game/last_played", current_level)
+	ProjectSettings.save()
 
 # Check if current win beats previous best, and save it if so
 func record_win(score, time):
 	# Get current difficulty level
-	var diff = pers_opts["difficulty"]
+	var diff = ProjectSettings.get_setting("game/difficulty")
 	# If no previous best exists, write a new one
-	if !user_data.has(str(current_level)):
-		user_data[str(current_level)] = {}
-		user_data[str(current_level)][diff] = {"score":score,"time":time}
+	if !user_data.has(globals.current_level):
+		user_data[globals.current_level] = {}
+		user_data[globals.current_level][diff] = {"score":score,"time":time}
 		FileIO.write_json_file(user_filepath, user_data)
 	# If no previous record exists for current difficulty, write one
-	elif !user_data[str(current_level)].has(diff):
-		user_data[str(current_level)][diff] = {"score":score,"time":time}
+	elif !user_data[globals.current_level].has(diff):
+		user_data[globals.current_level][diff] = {"score":score,"time":time}
 		FileIO.write_json_file(user_filepath, user_data)
 	# If score beats record best, overwrite it
-	elif score < user_data[str(current_level)][diff]["score"]:
-		user_data[str(current_level)][diff]["score"] = score
-		user_data[str(current_level)][diff]["time"] = time
+	elif score < user_data[globals.current_level][diff]["score"]:
+		user_data[globals.current_level][diff]["score"] = score
+		user_data[globals.current_level][diff]["time"] = time
 		FileIO.write_json_file(user_filepath, user_data)
 	# If score ties and time beats record best, overwrite it
-	elif score == user_data[str(current_level)][diff]["score"] and time < user_data[str(current_level)][diff]["time"]:
-		user_data[str(current_level)][diff]["time"] = time
+	elif score == user_data[globals.current_level][diff]["score"] and time < user_data[globals.current_level][diff]["time"]:
+		user_data[globals.current_level][diff]["time"] = time
 		FileIO.write_json_file(user_filepath, user_data)
 
 # Count the number of files in a given directory
