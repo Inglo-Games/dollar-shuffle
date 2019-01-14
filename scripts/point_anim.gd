7cac8c9d14b0ed63dfcbb196e15e1eb9c5c3b23e
Brett Haines <bhaines3@gatech.edu>
2019-01-14 18:06:12 -0500

    Tweaking point animation positioning

--- scripts/point_anim.gd
+++ scripts/point_anim.gd
@@ -3,7 +3,7 @@ extends Sprite
 # Speed the point should move
 const percent_per_sec = 1.45
 # Distance to destination before dissapating
-const limit_dist = 150.0
+const limit_dist = 128.0
 
 # Node locations
 var loc_start = Vector2(0,0)
@@ -15,13 +15,17 @@ func _ready():
 	set_process(true)
 	# Setup texture
 	texture = load("res://assets/icons/dollar.png")
+	# Center sprite
+	centered = true
+	# Show animation behind nodes and lines
+	show_behind_parent = true
 	# Remain invisible until movement starts
 	visible = false
 
 func _process(delta):
 	# Clear node if it reached its end location
 	var dist = loc_end.distance_to(position)
-	if dist < limit_dist:
+	if dist < limit_dist * scale.x:
 		visible = false
 		queue_free()
 	# Else move it closer to destination node
@@ -32,6 +36,6 @@ func _process(delta):
 
 func init(node1, node2):
 	# Save the start and end locations
-	loc_start = node1.rect_position
-	loc_end = node2.rect_position
+	loc_start = node1.rect_position + (node1.rect_size * node1.rect_scale * 0.5)
+	loc_end = node2.rect_position + (node2.rect_size * node2.rect_scale * 0.5)
 
